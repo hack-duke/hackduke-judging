ac6930327a399c1a2c1f7734cee5cd275aaa7d09
1heart <yixin1996@gmail.com>
2016-08-08 00:19:36 -0700

    Fix vote distribution bug

--- judging_sessions.py
+++ judging_sessions.py
@@ -3,16 +3,21 @@ import queue
 CHOICE_A, CHOICE_B = 'CHOICE_A', 'CHOICE_B'
 
 class JudgingSession():
+
     def __init__(self, num_alts):
         self.num_alts = num_alts
+
     def get_decision(self):
         raise NotImplementedError
+
     def perform_decision(self):
         raise NotImplementedError
+
     def get_results(self):
         raise NotImplementedError
 
 class SimpleSession(JudgingSession):
+
     def __init__(self, num_alts):
         self.num_alts = num_alts
         self.curr_judges, self.votes, self.num_times_judged = {}, {}, {}
@@ -21,12 +26,14 @@ class SimpleSession(JudgingSession):
             self.votes[i] = 0
             self.num_times_judged[i] = 0
             self.q.put((0, i))
+
     def get_decision(self, judge_id):
         if judge_id in self.curr_judges:
             return self.curr_judges[judge_id]
         a,b  = self.q.get()[1], self.q.get()[1]
         self.curr_judges[judge_id] = (a, b)
         return (a,b)
+
     def perform_decision(self, judge_id, favored):
         if judge_id not in self.curr_judges:
             return 'You are currently not judging!'
@@ -37,10 +44,11 @@ class SimpleSession(JudgingSession):
             self.votes[a] += 1
         else:
             self.votes[b] += 1
-        self.q.put((self.votes[a], a))
-        self.q.put((self.votes[b], b))
+        self.num_times_judged[a] += 1; self.num_times_judged[b] += 1
+        self.q.put((self.num_times_judged[a], a)); self.q.put((self.num_times_judged[b], b))
         self.curr_judges.pop(judge_id)
         return ''
+
     def get_results(self):
         return self.votes
 

5aedfe83e3a5a6a747418c998fda9242e066882a
Andy <ownz.andy@gmail.com>
2016-08-17 00:14:45 -0500

    refactoring save_session

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ from redis_store import RedisStore
 
 app = Flask(__name__)
 
+redis_retry_limit = 3
 store = RedisStore()
 
 @app.route("/")
@@ -12,7 +13,6 @@ def hello():
 
 @app.route("/init", methods = ['POST'])
 def init_judge_session():
-    print("IN INIT JUDGE")
     result = dict()
     json_args = request.get_json()
     if 'num_alts' not in json_args:
@@ -30,24 +30,22 @@ def init_judge_session():
     return jsonify(result)
 
 @app.route('/get_decision', methods = ['POST'])
-def get_decision():
-    result = dict()
+def get_decision(num=0):
+    success = dict()
+    error = dict()
     json_args = request.get_json()
     if 'judge_id' not in json_args:
         result['error'] = 'Not enough args'
         return jsonify(result)
     judge_id = json_args['judge_id']
     a,b = g.curr_session.get_decision(judge_id)
-    result['choice_a'] = a
-    result['choice_b'] = b
-    if store.save_session(g.curr_session, g.session_name) is None:
-        before_request()
-        return get_decision()
-    else:
-        return jsonify(result)
+    success['choice_a'] = a
+    success['choice_b'] = b
+    error['error'] = 'Server overloaded, try again later'
+    return save_session(success, error, get_decision, num)
 
 @app.route('/perform_decision', methods = ['POST'])
-def perform_decision():
+def perform_decision(num=0):
     result = dict()
     json_args = request.get_json()
     if 'judge_id' not in json_args or 'favored' not in json_args:
@@ -55,17 +53,11 @@ def perform_decision():
         return jsonify(result)
     judge_id, favored = json_args['judge_id'], json_args['favored']
     result['error'] = g.curr_session.perform_decision(judge_id, favored)
-    if store.save_session(g.curr_session, g.session_name) is None:
-        before_request()
-        return perform_decision()
-    else:
-        return jsonify(result)
-    return jsonify(result)
+    return save_session(result, result, perform_decision, num)
 
 @app.route('/results', methods = ['POST'])
 def results():
     result = dict()
-    json_args = request.get_json()
     result.update(g.curr_session.get_results())
     result['error'] = ''
     return jsonify(result)
@@ -81,10 +73,22 @@ def before_request():
         result['error'] = 'Need session name'
         return jsonify(result)
     g.session_name = json_args['session_name']
-    g.curr_session = store.get_curr_session(g.session_name)
+    update_session()
     if g.curr_session is None and not request.path == '/init':
         result['error'] = 'Need to init first!'
         return jsonify(result)
+
+def update_session():
+    g.curr_session = store.get_curr_session(g.session_name)
+
+def save_session(success, error, route, num):
+    if store.save_session(g.curr_session, g.session_name) is None:
+        if num > redis_retry_limit:
+            return jsonify(error)
+        update_session()
+        return route(num+1)
+    else:
+        return jsonify(success)
 
 if __name__ == "__main__":
     app.run()
